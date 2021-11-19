31323675375e8d9bde06c5cf1685476a2882ef13
Daniel Bou De Souza <T807314@tokiomarine.com.br>
2021-11-19 14:58:34 -0300

    Chain of Responsability

---
+++ loja/src/TesteDescontos.java
@@ -0,0 +1,20 @@
+import java.math.BigDecimal;
+
+import br.com.alura.loja.imposto.CalculadoraDeDescontos;
+import br.com.alura.loja.imposto.ICMS;
+import br.com.alura.loja.orcamento.Orcamento;
+
+public class TesteDescontos {
+
+	public static void main(String[] args) {
+		
+		CalculadoraDeDescontos calculadora = new CalculadoraDeDescontos();
+		
+		Orcamento primeiro = new Orcamento(new BigDecimal("200"), 6);
+		System.out.println(calculadora.calcular(primeiro));
+		
+		Orcamento segundo = new Orcamento(new BigDecimal("1000"), 1);
+		System.out.println(calculadora.calcular(segundo));
+		
+	}
+}
